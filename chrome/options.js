54b8359f8b4b5419dcf72d47bf89f0e03684f459
FatMinMin <chiehmin18@gmail.com>
2016-09-01 15:38:41 +0800

    add location options

--- chrome/options.js
+++ chrome/options.js
@@ -1,6 +1,13 @@
 $(function() {
+  // areas settings
+  let areas = JSON.parse(localStorage["areas"]);
+  $("#opt_tp").prop("checked", areas.tp);
+  $("#opt_hc").prop("checked", areas.hc);
+  $("#opt_kh").prop("checked", areas.kh);
+
+  // pokemons settings
   let container = $("#pokemons-selection");
-  notifyPokemons = JSON.parse(localStorage["pokemons"]);
+  let notifyPokemons = JSON.parse(localStorage["pokemons"]);
   content = '';
   for(let i = 0; i < pokemons.length; i++) {
     let pokemon = pokemons[i];
@@ -21,14 +28,24 @@ $(function() {
     $(`#id-${key}`).prop("checked", notifyPokemons[key]);
   }
 
+
+
   $("#btnSave").click(function() {
+    // area settings
+    areas.tp = $("#opt_tp").prop("checked");
+    areas.hc = $("#opt_hc").prop("checked");
+    areas.kh = $("#opt_kh").prop("checked");
+    localStorage["areas"] = JSON.stringify(areas);
+
+    // pokemons settings
     for(let i = 1; i <= pokemons.length; i++) {
       let isChecked = $(`#id-${i}`).prop("checked");
       if(isChecked) notifyPokemons[i] = true;
       else notifyPokemons[i] = false;
     }
     localStorage["pokemons"] = JSON.stringify(notifyPokemons);
-    chrome.runtime.sendMessage({request: "reloadWanted"});
+    
+    chrome.runtime.sendMessage({request: "reloadSettings"});
     alert("儲存完成");
   });
 });
